40bf77dfd473aa3593568c451aa8514cf9afb690
AnishGarg1 <anishgarg805@gmail.com>
2024-06-09 23:57:57 +0530

    server update

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,10 +1,47 @@
-exports.signup = (req, res) => {
-    const {username, firstName, lastName, email, password} = req.body;
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-    if(!username || !firstName || !lastName || !email || !password){
-        return res.status(400).json({
-            success: false,
-            message: "All fields are required",
-        })
+exports.signup = async (req, res) => {
+  try {
+    const { username, firstName, lastName, email, password } = req.body;
+
+    if (!username || !firstName || !lastName || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
-}
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(401).json({
+        success: false,
+        message: "User already exits",
+      });
+    }
+
+    // password hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+
+    return res.status(200).json({
+      success: true,
+      user,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.log("Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error, try again",
+    });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -3,12 +3,25 @@ const app = express();
 const dotenv = require("dotenv");
 const database = require("./config/database");
 
+const userRoutes = require("./routes/User");
+
 // Connecting to database
 database.dbConnect();
 
 dotenv.config(); // parsing .env file variables
 
+// middlewares
+app.use(express.json());
+
 const port = process.env.PORT || 4000;
+
+// routes
+app.use("/api/v1/auth", userRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Rehaabit Task App");
+})
+
 app.listen(port, () => {
     console.log(`App is listerning on port ${port}`);
 })
