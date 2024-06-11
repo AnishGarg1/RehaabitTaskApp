fb7d4fa6490b2d9caaa60e0a34ddaee8f07213f7
AnishGarg1 <anishgarg805@gmail.com>
2024-06-12 00:45:50 +0530

    server updated

--- server/controllers/Task.js
+++ server/controllers/Task.js
@@ -55,6 +55,50 @@ exports.createTask = async (req, res) => {
 };
 
 // update task
+exports.updateTask = async (req, res) => {
+    try {
+        const { taskId, title, description, status } = req.body;
+        
+        if(!taskId){
+            return res.status(400).json({
+                success: false,
+                message: "Please fill all details",
+            });
+        }
+
+        const task = await Task.findById(taskId);
+        if(!task){
+            return res.statusa(404).json({
+                success: false,
+                message: "Task not found",
+            });
+        }
+
+        if(title){
+            task.title = title;
+        }
+        if(description){
+            task.description = description;
+        }
+        if(status){
+            task.status = status;
+        }
+
+        await task.save();
+
+        return res.status(200).json({
+            success: true,
+            task,
+            message: "Task updated successfully",
+        });
+    } catch (error) {
+        console.log("Error:", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error, try again",
+        });
+    }
+}
 
 // get task details
 exports.getTaskDetails = async (req, res) => {
@@ -69,6 +113,8 @@ exports.getTaskDetails = async (req, res) => {
         }
 
         const task = await Task.findById(taskId).populate({path: "user"});
+
+        task.user.password = undefined;
 
         if(!task){
             return res.status(404).json({
--- server/routes/Task.js
+++ server/routes/Task.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createTask, deleteTask, getAllTaskDetails, getTaskDetails } = require("../controllers/Task");
+const { createTask, deleteTask, getAllTaskDetails, getTaskDetails, updateTask } = require("../controllers/Task");
 const { auth } = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/createTask", auth, createTask);
 router.get("/getTask", auth, getTaskDetails);
+router.get("/updateTask", auth, updateTask);
 router.get("/getAllTasks", auth, getAllTaskDetails);
 router.delete("/deleteTask", auth, deleteTask);
 
