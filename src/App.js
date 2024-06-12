41a6011b36919d4adcb65625b4b8375406ccd70d
AnishGarg1 <anishgarg805@gmail.com>
2024-06-12 22:34:34 +0530

    redux state & routing update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './component/Home';
+import Error from './component/Error';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      Rehaabit Task App
+    <div className="w-screen min-h-screen">
+      <Navbar/>
+      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={"login"}/>
+        <Route path='/signup' element={"signup"}/>
+        <Route path='/dashboard' element={"dashboard"}/>
+        <Route path='/task/:id' element={"task"}/>
+        <Route path='*' element={<Error/>}/>
+      </Routes>
     </div>
   );
 }
