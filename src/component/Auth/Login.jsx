ecedb767d9489948423f6b5aacdf69e980b88986
AnishGarg1 <anishgarg805@gmail.com>
2024-06-18 23:15:23 +0530

    project done

--- src/component/Auth/Login.jsx
+++ src/component/Auth/Login.jsx
@@ -27,54 +27,57 @@ const Login = () => {
     }
 
   return (
-    <div>
-        <form
-            onSubmit={handleSubmit}
-            className='w-full'
-        >
-            <div>
-                <label
-                    htmlFor='email'
-                    className='text-xs'
-                >
-                    Email
-                </label>
-                <input
-                    required
-                    id='email'
-                    type='text'
-                    name='email'
-                    value={email}
-                    onChange={handleChangeForm}
-                    placeholder='Enter email'
-                    // className='w-full'
-                />
-            </div>
-            <div>
-                <label
-                    htmlFor='password'
-                    className='text-xs'
-                >
-                    Password
-                </label>
-                <input
-                    required
-                    id='password'
-                    type='password'
-                    name='password'
-                    value={password}
-                    onChange={handleChangeForm}
-                    placeholder='Enter Passowrd'
-                    // className='w-full'
-                />
-            </div>
-            <button
-                type='submit'
-                className='border-2'
+    <div className='flex items-center justify-center mt-10'>
+        <div className='bg-white p-8 rounded-lg shadow-lg max-w-md w-full'>
+            <h2 className='text-2xl font-semibold mb-4 text-center'>Login to Your Account</h2>
+            <form
+                onSubmit={handleSubmit}
+                className='space-y-6'
             >
-                Login
-            </button>
-        </form>
+                <div>
+                    <label
+                        htmlFor='email'
+                        className='text-sm block font-medium text-gray-700'
+                    >
+                        Email
+                    </label>
+                    <input
+                        required
+                        id='email'
+                        type='text'
+                        name='email'
+                        value={email}
+                        onChange={handleChangeForm}
+                        placeholder='Enter email'
+                        className='w-full border-2 px-3 py-2 rounded-lg focus:outline-none focus:border-blue-500'
+                    />
+                </div>
+                <div>
+                    <label
+                        htmlFor='password'
+                        className='text-sm block font-medium text-gray-700'
+                    >
+                        Password
+                    </label>
+                    <input
+                        required
+                        id='password'
+                        type='password'
+                        name='password'
+                        value={password}
+                        onChange={handleChangeForm}
+                        placeholder='Enter Passowrd'
+                        className='w-full border-2 px-3 py-2 rounded-lg focus:outline-none focus:border-blue-500'
+                    />
+                </div>
+                <button
+                    type='submit'
+                    className='w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors focus:outline-none'
+                >
+                    Login
+                </button>
+            </form>
+        </div>
     </div>
   )
 }
--- src/component/Auth/Signup.jsx
+++ src/component/Auth/Signup.jsx
@@ -52,114 +52,156 @@ const Signup = () => {
       setIsPassMatch(false);
     }
   }, [password, confirmPassword])
+  
 
   return (
-    <div>
-      <div>Create Your Account</div>
-      <form
-        className='border-2'
-        onSubmit={handleSubmit}
-      >
-          <label>
-            <p>Username</p>
-            <input
-              required
-              type='text'
-              name='username'
-              value={username}
-              className='border-2'
-              placeholder='Enter username'
-              onChange={handleChangeForm}
-            />
-          </label>
-
-          <div>
-            <label>
-              <p>First Name</p>
+    <div className='flex items-center justify-center mt-10'>
+      <div className='bg-white p-8 rounded-lg shadow-lg max-w-md w-full'>
+        <h2 className='text-2xl font-semibold mb-4 text-center'>
+          Create Your Account
+        </h2>
+        <form
+          onSubmit={handleSubmit}
+        >
+          <div className='space-y-4'>
+            <div>
+              <label
+                htmlFor='username'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Username
+              </label>
               <input
                 required
                 type='text'
-                name='firstName'
-                value={firstName}
-                className='border-2'
-                placeholder='Enter first name'
+                id='username'
+                name='username'
+                value={username}
+                className='border-2 w-full px-3 py-2 rounded-lg focus:outline-none focus:border-blue-500'
+                placeholder='Enter username'
                 onChange={handleChangeForm}
               />
-            </label>
+            </div>
+
+            <div className='grid grid-cols-2 gap-4'>
+              <div>
+                  <label
+                    htmlFor='firstName'
+                    className='block text-sm font-medium text-gray-700'
+                  >
+                    First Name
+                  </label>
+                  <input
+                    required
+                    type='text'
+                    id='firstName'
+                    name='firstName'
+                    value={firstName}
+                    className='border-2 w-full px-3 py-2 rounded-lg focus:outline-none focus:border-blue-500'
+                    placeholder='Enter first name'
+                    onChange={handleChangeForm}
+                  />
+              </div>
+              
+              <div>
+                <label
+                  htmlFor='lastName'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  Last Name
+                </label>
+                <input
+                  required
+                  id='lastName'
+                  type='text'
+                  name='lastName'
+                  value={lastName}
+                  className='border-2 w-full px-3 py-2 rounded-lg focus:outline-none focus:border-blue-500'
+                  placeholder='Enter last name'
+                  onChange={handleChangeForm}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor='email'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Email
+              </label>
+                <input
+                  required
+                  type='email'
+                  id='email'
+                  name='email'
+                  value={email}
+                  className='border-2 w-full px-3 py-2 rounded-lg focus:outline-none focus:border-blue-500'
+                  placeholder='Enter email'
+                  onChange={handleChangeForm}
+                />
+            </div>
             
-            <label>
-              <p>Last Name</p>
-              <input
-                required
-                type='text'
-                name='lastName'
-                value={lastName}
-                className='border-2'
-                placeholder='Enter last name'
-                onChange={handleChangeForm}
-              />
-            </label>
-          </div>
-
-          <label>
-            <p>Email</p>
-            <input
-              required
-              type='email'
-              name='email'
-              value={email}
-              className='border-2'
-              placeholder='Enter email'
-              onChange={handleChangeForm}
-            />
-          </label>
-          
-          <div>
-            <label>
-              <p>Password</p>
+            <div>
+              <label
+                htmlFor='password'
+                className='block text-sm font-medium text-gray-700'
+              >
+                Password
+              </label>
               <input
                 required
                 type='password'
+                id='password'
                 name='password'
                 value={password}
-                className='border-2'
+                className='border-2 w-full px-3 py-2 rounded-lg focus:outline-none focus:border-blue-500'
                 placeholder='Enter password'
                 onChange={handleChangeForm}
               />
-            </label>
+            </div>
 
-            <label>
-              <p>Confrim Password</p>
+            <div>
+              <div className='flex w-full items-center justify-between'>
+                <label
+                  htmlFor='confirmPassword'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  Confrim Password
+                </label>
+                {isPassMatch && (
+                  <div className='mr-1'>
+                    <FaRegCheckCircle className='text-green-500'/>
+                  </div>
+                )}
+              </div>
+
               <input
                 required
                 type='password'
+                id='confirmPassword'
                 name='confirmPassword'
                 value={confirmPassword}
-                className='border-2'
+                className='border-2 w-full px-3 py-2 rounded-lg focus:outline-none focus:border-blue-500'
                 placeholder='Enter confirm password'
                 onChange={handleChangeForm}
               />
-            </label>
+            
+            </div>
 
             <div>
-              {isPassMatch && (
-                <span>
-                  <FaRegCheckCircle className='text-green-500'/>
-                </span>
-              )}
+              <button
+                type='submit'
+                className='w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors focus:outline-none'
+              >
+                Signup
+              </button>
             </div>
           </div>
-          <div>
-            <button
-            type='submit'
-              className='border-2'
-            >
-              Signup
-            </button>
-          </div>
-      </form>
+        </form>
+        </div>
     </div>
   )
 }
 
-export default Signup
+export default Signup;
