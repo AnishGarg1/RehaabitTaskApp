c8121ebb83341fb340c9aaca8e0c54057adf42ea
AnishGarg1 <anishgarg805@gmail.com>
2024-06-19 08:31:04 +0530

    project done

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const database = require("../config/database");
+const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors")
 
-const userRoutes = require("../routes/User");
-const taskRoutes = require("../routes/Task");
+const userRoutes = require("./routes/User");
+const taskRoutes = require("./routes/Task");
 
 // Connecting to database
 database.dbConnect();
@@ -17,7 +17,7 @@ dotenv.config(); // parsing .env file variables
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://rehaabit-task.netlify.app/",
+    origin: ["http://localhost:3000", "https://rehaabit-task.netlify.app"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
